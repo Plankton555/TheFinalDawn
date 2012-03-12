ecee4002ddfd471cddfafeb9a527582545984c6a
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-12 19:28:12 +0100

    Lagt projektet i en projektmapp fr sig

---
+++ Project/src/Main.java
@@ -0,0 +1,18 @@
+import javax.swing.JFrame;
+
+
+public class Main {
+
+	static JFrame frame = new JFrame("Test frame");
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		frame.setSize(640, 480);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+	}
+
+}
