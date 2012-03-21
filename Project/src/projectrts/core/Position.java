0f47c73b6be36a20994ee5a021ab26249a998af1
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-21 09:16:48 +0100

    1st implementation of Position
    Implemented a simple immutable version of the class Position.

--- Project/src/projectrts/core/Position.java
+++ Project/src/projectrts/core/Position.java
@@ -1,16 +1,39 @@
 package projectrts.core;
 
+/**
+ * A 2D position.
+ * @author Bjorn Persson Mattsson
+ *
+ */
 public class Position {
 
-	public float getX()
+	private float x;
+	private float y;
+	
+	/**
+	 * Creates a new position with the given components.
+	 * @param x X component.
+	 * @param y Y component.
+	 */
+	public Position(float x, float y)
 	{
-		// TODO
-		return 0;
+		this.x = x;
+		this.y = y;
 	}
 	
+	/**
+	 * @return The x component.
+	 */
+	public float getX()
+	{
+		return x;
+	}
+	
+	/**
+	 * @return The y component.
+	 */
 	public float getY()
 	{
-		// TODO
-		return 0;
+		return y;
 	}
 }
