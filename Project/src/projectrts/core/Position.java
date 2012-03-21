38e1eacfc71a55b3d074e62ef489ead29b1f4006
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-21 15:11:42 +0100

    Changes to Position
    Added new Position constructor that takes another Position.
    Added a clone() method.
    Added an equals() method (hashCode() generated by Eclipse).

--- Project/src/projectrts/core/Position.java
+++ Project/src/projectrts/core/Position.java
@@ -22,6 +22,48 @@ public class Position {
 	}
 	
 	/**
+	 * Creates a new position from another position.
+	 * @param otherPos Other position
+	 */
+	public Position(Position otherPos)
+	{
+		this(otherPos.getX(), otherPos.getY());
+	}
+	
+	/**
+	 * @return Returns a clone of this position.
+	 */
+	public Position clone()
+	{
+		return new Position(this.x, this.y);
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31; // Default generated hashcode
+		int result = 1;
+		result = prime * result + Float.floatToIntBits(x);
+		result = prime * result + Float.floatToIntBits(y);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Position other = (Position) obj;
+		if (Float.floatToIntBits(x) != Float.floatToIntBits(other.x))
+			return false;
+		if (Float.floatToIntBits(y) != Float.floatToIntBits(other.y))
+			return false;
+		return true;
+	}
+
+	/**
 	 * @return The x component.
 	 */
 	public float getX()
