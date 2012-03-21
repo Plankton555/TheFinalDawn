41fdcdbaf684de577d4f68bbf9a01d1f07988eb7
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-21 15:37:11 +0100

    Small changes in class P

--- Project/src/projectrts/core/P.java
+++ Project/src/projectrts/core/P.java
@@ -1,17 +1,18 @@
 package projectrts.core;
 
-public enum P {INSTANCE;
-
-public int getWorldWidth() {
-	return 100;
-}
-
-public int getWorldHeight() {
-	return 100;
-}
-
-public float getTileSideLength() {
-	return 0.05f;
-}
-
+public enum P {
+	
+	INSTANCE;
+	
+	public int getWorldWidth() {
+		return 100;
+	}
+	
+	public int getWorldHeight() {
+		return 100;
+	}
+	
+	public float getTileSideLength() {
+		return 0.05f;
+	}
 }
