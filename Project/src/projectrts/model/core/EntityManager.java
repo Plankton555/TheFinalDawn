f576e6ab0053a618f30c50df47524dca6e0159d6
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-23 12:58:39 +0100

    Added getNearbyEntities() in EntityManager

--- Project/src/projectrts/model/core/EntityManager.java
+++ Project/src/projectrts/model/core/EntityManager.java
@@ -1,10 +1,38 @@
 package projectrts.model.core;
 
+import java.util.List;
+
+/**
+ * The singleton entity manager.
+ * @author Bjorn Persson Mattsson
+ *
+ */
 public class EntityManager {
 
+	private static EntityManager instance = new EntityManager();
 	
+	/**
+	 * @return The instance of this class.
+	 */
+	public static EntityManager getInstance()
+	{
+		return instance;
+	}
+	
+	/**
+	 * Returns a list of all entities close to the position.
+	 * Returns all entities that can be seen from the circle with center in 'p'
+	 * and the radius 'range'.
+	 * @param p The position
+	 * @param range The distance from the position.
+	 * @return List of all entities close to position.
+	 */
+	public List<IEntity> getNearbyEntities(Position p, float range)
+	{
+		// TODO Implement this
+		return null;
+	}
 	/*
-	singleton
 	mappnign
 	*/
 }
