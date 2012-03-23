3ca39a6ba16ced05370f2308daac3779d04270f3
Markus Ekstrom <markus_xtrom@hotmail.com>
2012-03-23 08:21:54 +0100

    Integrated old work in the new structure
    Deleted AppStates, adding InGameState in it's place. Moved Constants to
    global package.

--- Project/src/projectrts/Main.java
+++ Project/src/projectrts/Main.java
@@ -5,8 +5,6 @@ package projectrts;
 import projectrts.controller.*;
 
 public class Main {
-
-	//TODO This class should only contain main method
     public static void main(String[] args) {
         AppController app = new AppController();
         app.start();
--- Project/src/projectrts/controller/AppStates.java
+++
@@ -1,7 +0,0 @@
-package projectrts.controller;
-
-public class AppStates {
-
-	
-	// TODO Implement this class
-}
