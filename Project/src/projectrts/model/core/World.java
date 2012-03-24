d83dd7ef8fe8c51f59db28ff6ced706b70ed92d5
Bjorn Mattsson <plankton.ollas@gmail.com>
2012-03-24 21:01:13 +0100

    Added node initialization in World

--- Project/src/projectrts/model/core/World.java
+++ Project/src/projectrts/model/core/World.java
@@ -2,9 +2,11 @@ package projectrts.model.core;
 
 public class World {
 	private ITile[][] tileMap;
+	private Node[][] nodes;
 	
 	public World(int height, int width) {
 		initializeTileMap(height, width);
+		initNodes(height, width);
 	}
 	
 	private void initializeTileMap(int height, int width) {
@@ -17,6 +19,48 @@ public class World {
 		}
 	}
 	
+	private void initNodes(int height, int width)
+	{
+		nodes = new Node[height+1][width+1];
+		
+		// Creates a matrix of nodes with the upper left at position (0,0)
+		// and the lower right at position (width,height)
+		for (int i = 0; i <= height; i++)
+		{
+			for (int j = 0; j <= width; j++)
+			{
+				nodes[i][j] = new Node(j, i);
+			}
+		}
+		createNodeConnections();
+	}
+	
+	private void createNodeConnections()
+	{
+		for (int i=0; i<=nodes.length; i++)
+		{
+			for (int j=0; j<=nodes[i].length; j++)
+			{
+				// Add all nodes surrounding this node
+				for (int k=i-1; k<=i+1; k++)
+				{
+					// If the row exists
+					if (k >= 0 && k < nodes.length)
+					{
+						for (int l=j-1; l<=j+1; l++)
+						{
+							// If the column exists
+							if (l >= 0 && l < nodes[i].length)
+							{
+								nodes[i][j].addNeighbour(nodes[k][l]);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 	public ITile[][] getTileMap()
 	{
 		return tileMap;
