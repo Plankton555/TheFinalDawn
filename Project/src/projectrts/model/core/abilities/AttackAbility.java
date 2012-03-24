1d81edc41fa32034ace724bc5d705ab5280e1510
AftonTroll <filbry_92@hotmail.com>
2012-03-24 08:22:26 +0100

    Added simple functionality for the AttackAbility

--- Project/src/projectrts/model/core/abilities/AttackAbility.java
+++ Project/src/projectrts/model/core/abilities/AttackAbility.java
@@ -1,6 +1,7 @@
 package projectrts.model.core.abilities;
 
 import projectrts.model.core.entities.IPlayerControlledEntity;
+import projectrts.model.core.entities.PlayerControlledEntity;
 
 /**
  * An ability for attacking
@@ -15,8 +16,15 @@ public class AttackAbility implements IAbility {
 	}
 	
 	public void doAbility(IPlayerControlledEntity attacker, IPlayerControlledEntity target){
+		
 		//TODO: Implement functionality for this ability
 		//Check if possible? Should it be check multiple times (Here + in AI)?
+		//The amount of dmg should be attacker.getDamage()
+		
+		if(target instanceof PlayerControlledEntity){
+			PlayerControlledEntity entityTarget = (PlayerControlledEntity) target;
+			entityTarget.takeDamage(50);
+		}
 	}
 	
 	
