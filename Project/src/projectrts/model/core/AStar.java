af260a72d48aa16c6c7d7cb37ed8ba6a73befb9b
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-28 17:30:20 +0200

    AStar class is now singleton

--- Project/src/projectrts/model/core/AStar.java
+++ Project/src/projectrts/model/core/AStar.java
@@ -7,7 +7,39 @@ package projectrts.model.core;
  */
 public class AStar {
 
+	private static AStar instance;
+	private World world;
+	
+	private AStar(World world)
+	{
+		this.world = world;
+	}
+	
+	/**
+	 * Initializes the singleton A* class
+	 * @param world Game world
+	 */
+	public static void initialize(World world)
+	{
+		instance = new AStar(world);
+	}
+	
+	/**
+	 * @return Singleton instance.
+	 * @throws IllegalStateException If not yet initialized.
+	 */
+	public static AStar getInstance() throws IllegalStateException
+	{
+		if (instance == null)
+		{
+			throw new IllegalStateException("You must initialize this class before you can use it");
+		}
+		return instance;
+	}
+	
 	public void calculatePath(Position startPos, Position targetPos)
 	{
+		Node startNode = world.getNodeAt(startPos);
+		Node endNode = world.getNodeAt(targetPos);
 	}
 }
