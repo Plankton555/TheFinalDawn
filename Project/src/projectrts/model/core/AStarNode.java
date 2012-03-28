b086e325548a82c8ef28f73cef2dac9a495ff72b
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-28 18:14:49 +0200

    Changes to AStarNode
    AStarNode no longer extends Node, now it has a Node instead.
    Added getNeighbours() in AStarNode.
    Added sameNodeAs() in AStarNode.
    Added some javadoc.

--- Project/src/projectrts/model/core/AStarNode.java
+++ Project/src/projectrts/model/core/AStarNode.java
@@ -1,13 +1,38 @@
 package projectrts.model.core;
 
-public class AStarNode extends Node implements Comparable<AStarNode> {
+import java.util.ArrayList;
+import java.util.List;
+
+public class AStarNode implements Comparable<AStarNode> {
 	
+	private Node node;
 	private float cost = 0;
+	
+	/**
+	 * Creates a new AStarNode that refers to the provided node.
+	 * @param node Node
+	 */
 	public AStarNode(Node node)
 	{
-		super(node.getPosition());
+		this.node = node;
 	}
 	
+	public List<AStarNode> getNeighbours()
+	{
+		List<AStarNode> output = new ArrayList<AStarNode>();
+		List<Node> neighbours = node.getNeighbours();
+		
+		for (Node n : neighbours)
+		{
+			output.add(new AStarNode(n));
+		}
+		return output;
+	}
+	
+	/**
+	 * Sets the cost for this node. AStarNodes are sorted by cost.
+	 * @param cost Cost
+	 */
 	public void setCost(float cost)
 	{
 		this.cost = cost;
@@ -17,5 +42,14 @@ public class AStarNode extends Node implements Comparable<AStarNode> {
 	public int compareTo(AStarNode other) {
 		// Need to negate since the lower the cost, the higher the node should be sorted.
 		return -Float.compare(cost, other.cost);
+	}
+	
+	/**
+	 * @param other Another AStarNode
+	 * @return true if both AStarNodes refer to the same Node, otherwise false.
+	 */
+	public boolean sameNodeAs(AStarNode other)
+	{
+		return node.equals(other.node);
 	}
 }
