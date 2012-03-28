8f8372266134a4b22eadbc38d371aae82d2ae468
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-03-28 17:54:15 +0200

    Added an AStarNode class that extends Node

---
+++ Project/src/projectrts/model/core/AStarNode.java
@@ -0,0 +1,22 @@
+package projectrts.model.core;
+
+public class AStarNode extends Node implements Comparable<AStarNode> {
+	
+	private float cost = 0;
+	public AStarNode(Node node)
+	{
+		super(node.getPosition());
+	}
+	
+	public void setCost(float cost)
+	{
+		this.cost = cost;
+	}
+	
+	@Override
+	public int compareTo(AStarNode other) {
+		// Need to negate since the lower the cost, the higher the node should be sorted.
+		return -Float.compare(cost, other.cost);
+	}
+
+}
