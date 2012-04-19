9739869bc691b98124cdf8e7e20189ad04ad7f3c
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-04-19 11:59:22 +0200

    Added a TODO

--- Project/src/projectrts/view/controls/AbstractControl.java
+++ Project/src/projectrts/view/controls/AbstractControl.java
@@ -6,6 +6,7 @@ import projectrts.model.core.entities.IEntity;
  * @author Markus Ekström
  *
  */
+// TODO Markus: Class names shouldn't shadow simple name of superclass
 public abstract class AbstractControl extends com.jme3.scene.control.AbstractControl{
 	
 	// TODO Markus: Add javadoc
