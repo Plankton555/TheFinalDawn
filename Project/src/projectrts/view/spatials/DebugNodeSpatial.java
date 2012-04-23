a145420ca1eb76c167aa5beb5c775471b66894b1
Bjorn Persson Mattsson <plankton.ollas@gmail.com>
2012-04-23 11:29:53 +0200

    Created a DebugNodeSpatial class
    Created and implemented the class DebugNodeSpatial.

---
+++ Project/src/projectrts/view/spatials/DebugNodeSpatial.java
@@ -0,0 +1,46 @@
+package projectrts.view.spatials;
+
+import projectrts.global.utils.MaterialManager;
+import projectrts.model.entities.IEntity;
+import projectrts.view.controls.ControlFactory;
+import projectrts.view.controls.MoveControl;
+
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.shape.Box;
+
+/**
+ * 
+ * @author Bjorn Persson Mattsson
+ *
+ */
+public class DebugNodeSpatial extends AbstractSpatial {
+private Material material;
+	
+	static {
+		SpatialFactory.INSTANCE.registerSpatial(DebugNodeSpatial.class.getSimpleName(), new DebugNodeSpatial("DebugNodeSpatialCreator", new Box()));
+	}
+	
+	private DebugNodeSpatial(String name, Box box) {
+		super(name, box);
+	}
+
+	/**
+	 * Creates and returns a new spatial using the parameters provided.
+	 * @param name The name of the new spatial.
+	 * @param material The material of the new spatial.
+	 * @param box The shape of the new spatial.
+	 * @param controlList A list of the controls the spatial will use.
+	 */
+	@Override
+	public AbstractSpatial createSpatial(String name, Box box, IEntity entity) {
+		material = MaterialManager.INSTANCE.getMaterial("Unshaded");
+		material.setColor("Color", ColorRGBA.Black);
+		
+		DebugNodeSpatial newSpatial = new DebugNodeSpatial(name, box);
+		newSpatial.setMaterial(material);
+		newSpatial.addControl(ControlFactory.INSTANCE.createControl(MoveControl.class.getSimpleName(), entity));
+		return newSpatial;
+	}
+
+}
