2247d66b35f282b9eb2451307cbd2329cf5e994e
AftonTroll <filbry_92@hotmail.com>
2012-05-02 16:43:19 +0200

    Added new icons for the GUI

--- Project/src/projectrts/global/utils/ImageManager.java
+++ Project/src/projectrts/global/utils/ImageManager.java
@@ -3,8 +3,12 @@ package projectrts.global.utils;
 import java.util.HashMap;
 
 import projectrts.model.entities.abilities.AttackAbility;
+import projectrts.model.entities.abilities.BuildWallAbility;
+import projectrts.model.entities.abilities.GatherResourceAbility;
 import projectrts.model.entities.abilities.MoveAbility;
 import projectrts.model.entities.abilities.OffensiveSpellAbility;
+import projectrts.model.entities.abilities.TrainWarriorAbility;
+import projectrts.model.entities.abilities.TrainWorkerAbility;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.render.NiftyImage;
 import de.lessvoid.nifty.render.NiftyRenderEngine;
@@ -29,9 +33,14 @@ public enum ImageManager {
 		NiftyRenderEngine engine = nifty.getRenderEngine();
 
 
-		images.put(MoveAbility.class.getSimpleName(), engine.createImage("assets/gui/MoveAbility.bmp", true));
-		images.put(AttackAbility.class.getSimpleName(), engine.createImage("/assets/gui/AttackAbility.bmp", false));
+		images.put(MoveAbility.class.getSimpleName(), engine.createImage("assets/gui/MoveAbility.png", false));
+		images.put(AttackAbility.class.getSimpleName(), engine.createImage("/assets/gui/AttackAbility.png", false));
 		images.put(OffensiveSpellAbility.class.getSimpleName(), engine.createImage("/assets/gui/OffensiveSpellAbility.bmp", false));
+		images.put(BuildWallAbility.class.getSimpleName(), engine.createImage("assets/gui/BuildWallAbility.png", false));
+		images.put(TrainWorkerAbility.class.getSimpleName(), engine.createImage("assets/gui/TrainWorkerAbility.png", false));
+		images.put(TrainWarriorAbility.class.getSimpleName(), engine.createImage("assets/gui/TrainWarriorAbility.png", false));
+		images.put(GatherResourceAbility.class.getSimpleName(), engine.createImage("assets/gui/GatherResourceAbility.png", true));
+		
 		images.put("NoImage", engine.createImage("/assets/gui/NoImage.bmp", false));
 		
 		NiftyImage image = engine.createImage("/assets/gui/background.png", false);
