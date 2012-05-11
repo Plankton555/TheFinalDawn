821f2a863e8e04c1df1c510148bfac4c75225891
Jakob Svensson <jakob-_-92@hotmail.com>
2012-05-11 13:05:20 +0200

    made deliverres and mineres to inner classes

--- Project/src/projectrts/model/abilities/IGatherAbility.java
+++
@@ -1,7 +0,0 @@
-package projectrts.model.abilities;
-
-// TODO Jakob: ADD JAVADOC!!!
-// TODO Jakob: PMD: An Interface should be used only to model a behaviour; consider converting this to a class.
-public interface IGatherAbility {
-	public static final int RESOURCE_CARRIED_AMOUNT = 12;
-}
